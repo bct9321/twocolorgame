b167260aa8f87a3c302bc7b18e23b101d72895ab
bct9321 <bct9321@rit.edu>
2019-11-23 11:34:14 -0500

    Basic Test for framework.
    On enter key the red box moves right.

--- src/body.php
+++ src/body.php
@@ -1,7 +1,11 @@
 <body>
 <!-- your content will go there..  -->
  
- 
+ <div id="gameWindow" style="background: yellow; width:200px; height:200px;">
+	<div id="player" style="background:red; width:20px; height:20px; position: absolute; left: 0; top: 0">
+		
+	</div>
+ </div>
  
 </body>
 </html>
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
     // jQuery code goes here
 	
+	function GameWindow() {
+		var $window = $('#gameWindow');
+	}
+
+	function GamePlayer() {
+		var $player = $('#player');
+		var x = 0;
+		var y = 0;
+		this.moveRight = function () {
+			x++;
+		}
+		this.render = function() {
+			$player.css('left', x);
+		}
+	}	
+	
+	
+	
+	
+	
+	var gameWindow = new GameWindow();
+	var player = new GamePlayer();
+	
+	$(document).on('keydown', function (even) {
+		if (event.keyCode === 13) {
+			player.moveRight();
+			player.render();
+		}
+	});
+	
+	
 });
